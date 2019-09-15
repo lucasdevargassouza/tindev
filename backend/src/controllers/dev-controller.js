03148ee90fba45c41d7857e7fc7a224abab68d75
Lucas DeVargas Souza <ls18017@gmail.com>
2019-09-15 12:40:34 -0300

    Criado a verificação se o usuário já existe!

--- backend/src/controllers/dev-controller.js
+++ backend/src/controllers/dev-controller.js
@@ -5,10 +5,20 @@ module.exports = {
     async store(req, res) {
         const { username } = req.body;
 
+        // Verifica se o usuário que está sendo cadastrado já existe.
+        const userExists = Dev.findOne({ user: username });
+
+        // Retorna o usuário caso ele já exista no sistema.
+        if (userExists) 
+            return res.json(userExists);
+
+        // Pega um usuário da api di github.
         const response = await axios.get(`https://api.github.com/users/${username}`);
 
+        // Separa as informações do usuário com um metodo novo do js. 
         const { name, bio, avatar_url } = response.data;
 
+        // Insere um usuário na base de dados. 
         const dev = await Dev.create({
             name: name != null ? name : "",
             user: username,
@@ -16,6 +26,7 @@ module.exports = {
             avatar: avatar_url
         });
 
+        // Retorna o usuário criado.
         return res.json(dev);
     }
 };
